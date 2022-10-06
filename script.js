4deb4d03654b9002d2e4d053f82cfaf7d06983de
branbeau <111940362+branbeau@users.noreply.github.com>
2022-10-05 20:31:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let pokemonList =
+[
+  {
+    "name": "Jigglypuff",
+    "height": "21",
+    "type":    [
+     	"grass",
+        "water"]
+  }
+]
+
+[
+  {    
+  	"name": "Butterfree",
+       "height": "12",
+       "type":   [
+     	  "grass",
+          "water"]
+  }
+]
+
+[
+  {    
+  	"name": "Lilipup",
+       "height": "7",
+       "type":   [
+     	  "grass",
+          "water"]
+  }
+];
