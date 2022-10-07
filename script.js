23eb2a84dbba31b4fde8436d962e47514938f8a0
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2022-10-07 12:26:39 -0700

    Updated /script.js

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-let pokemonList =
-[
-  {
-    "name": "Jigglypuff",
-    "height": "21",
-    "type":    [
-     	"grass",
-        "water"]
-  }
-]
-
-[
-  {    
-  	"name": "Butterfree",
-       "height": "12",
-       "type":   [
-     	  "grass",
-          "water"]
-  }
-]
-
-[
-  {    
-  	"name": "Lilipup",
-       "height": "7",
-       "type":   [
-     	  "grass",
-          "water"]
-  }
+let mypokemonList = [
+   {
+     "name": "Jigglypuff",
+     "height": "21",
+     "type": [
+        "grass",
+        "water"
+        ]
+    },
+    {
+      "name": "Butterfree",
+      "height": "12",
+      "type": [
+        "grass",
+        "water"
+        ]
+    },
+    {
+      "name": "Lilipup",
+      "height": "7",
+      "type": [
+        "grass",
+        "water"
+        ]
+    },
 ];
