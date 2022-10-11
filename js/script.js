1c85540d5ffd99f642d9e6bd54725e56c68fc5bd
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2022-10-11 13:32:41 -0700

    Added folders

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let mypokemonList = [
+   {
+     "name": "Jigglypuff",
+     "height": 21,
+     "type": [
+        "grass",
+        "water"
+        ]
+    },
+    {
+      "name": "Butterfree",
+      "height": 12,
+      "type": [
+        "grass",
+        "water"
+        ]
+    },
+    {
+      "name": "Lilipup",
+      "height": 7,
+      "type": [
+        "grass",
+        "water"
+        ]
+    },
+];
