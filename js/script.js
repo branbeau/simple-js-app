8b24da4008f9857c26e07e3ead66bdc2c0b16f4f
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2022-10-13 22:58:56 -0700

    Updated files

--- js/script.js
+++ js/script.js
@@ -1,26 +1,38 @@
 let mypokemonList = [
    {
      "name": "Jigglypuff",
-     "height": 21,
+     "height": 0.5,
      "type": [
-        "grass",
-        "water"
-        ]
+        "normal", "fairy"]
     },
     {
       "name": "Butterfree",
-      "height": 12,
-      "type": [
-        "grass",
-        "water"
-        ]
+      "height": 1.1,
+      "type": ["bug", "flying"]
     },
     {
       "name": "Lilipup",
-      "height": 7,
-      "type": [
-        "grass",
-        "water"
-        ]
+      "height": 0.4,
+      "type": ["normal"]
     },
 ];
+
+// Strings: ""
+// Arrays:[]
+// Objects: {}
+
+for (let i = 0; i <mypokemonList.length; i++) { 
+ if(mypokemonList[i].height <1.5 && mypokemonList.height >0.2 ) {
++ document.write(mypokemonList[i].name + "is" + Jigglypuff (height: 5) "Wow, that is big!<br>")
+ }
+ else if (mypokemonList.height <0.4){
+    document.write(mypokemonList[i].name + "kg!" "That is small!<br>");
+
+// Comments
+// Part 1: 
+// Line 28: The initialization is let i = 0. The conditional is i < mypokemonList.length. The action is i++.
+// Line 28-33: Create a for loop that iterates over each item in pokemonList
+// Part 2:
+// // Unable to see project in index.html file. 
+// Part 3:
+// Is line 26 a way to highlight special Pokémon on the list (ex. Jigglypuff (height: 5) ? 
