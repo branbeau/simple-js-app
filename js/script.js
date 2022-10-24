922b416f7db58a9f4428250e8537e0fc245380b6
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2022-10-24 13:42:13 -0700

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let mypokemonList = [
+let pokemonList = [
    {
      "name": "Jigglypuff",
      "height": 0.5,
@@ -21,15 +21,25 @@ let mypokemonList = [
 // Arrays:[]
 // Objects: {}
 
-for (let i=0; i < mypokemonList.length; i++){ 
- if (mypokemonList[i].height <0.8 && mypokemonList[i].height >0.2){
-     document.write(mypokemonList[i].name + " is " + mypokemonList[i].height + "m" + " That is average.<br>")
- }else {
-     document.write(mypokemonList[i].name + " is " + mypokemonList[i].height + "m" + " Wow, That is big!<br>");
-  }
-}; 
+// for (let i=0; i < pokemonList.length; i++){ 
+//  if (pokemonList[i].height <0.8 && pokemonList[i].height >0.2){
+//      document.write(pokemonList[i].name + " is " + pokemonList[i].height + "m" + " That is average.<br>")
+//  }else {
+//      document.write(pokemonList[i].name + " is " + pokemonList[i].height + "m" + " Wow, That is big!<br>");
+//   }
+// }; 
 
 // Comments
 // Part 1: 
 // Line 24: The initialization is let i=0. The conditional is i < mypokemonList.length. The action is i++.
 // Line 24-31: Create a for loop that iterates over each item in pokemonList
+
+// // Using forEach() loops:
+// pokemonList.forEach((document.write(pokemonList[i].name + " is " + pokemonList[i].height + "m" + " Wow, That is big!<br>");
+//  {
+//   document.write(name, height, type);
+// });
+
+pokemonList.forEach(function(pokemon) {
+    document.write(pokemon.name + " is " + pokemon.height + "m");
+   });
