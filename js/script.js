96309563182020013da2b4ccab0605580db52a2f
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2022-10-28 11:47:56 -0700

    Updated file

--- js/script.js
+++ js/script.js
@@ -32,21 +32,47 @@ let pokemonList = [
 // }; 
 
 // IIFE - Immediately Invoked Function Expression  
-let pokemonRepository = (function() { // Create a new pokemonRepository variable to hold what your IIFE will return and assign IIFE to the variable
-    let pokemonList = []; // Added pokemonList variable inside pokemonRepository IIFE (Wrap pokemonList array in an IIFE). IIFE wraps around the function - let pokemonRepository = (.
+let pokemonRepository = (function() { // New pokemonRepository variable holds what the IIFE will return and assign IIFE to the variable
+    
+    let pokemonList = [ // PokemonList variable inside pokemonRepository IIFE (Wrap pokemonList array in an IIFE). IIFE wraps around the function - let pokemonRepository = (.
+        { 
+            "name": "Jigglypuff", 
+            "height": 0.5, 
+            "type": ["normal", "fairy"]
+        },
+        {
+            "name": "Butterfree",
+            "height": 1.1,
+            "type": ["bug", "flying"]
+        },
+        {
+            "name": "Lilipup",
+            "height": 0.4,
+            "type": ["normal"]
+        }
+    ];
 
-    function getAll() {
-        return pokemonList;
+    function pokemonRepository.getAll() { // pokemonRepository.getAll() returns the pokemonList array
+    return pokemonList;
     }
 
-    function add(pokemon) {
-        pokemonList.push(pokemon);
+    function add(pokemonRepository.add(item) { // Add a single item to the pokemonList array (calling pokemonRepository.add(item); should add the Pokémon referred to with item to the pokemonList array) {
+        if (pokemonList[i].height < 0.8 && pokemonList[i].height > 0.2) {
+            document.write(pokemonList[i].name + " is " + pokemonList[i].height + "m" + " That is average.<br>") {
+                pokemonList.push(pokemon);
+            } else {
+                document.write(pokemonList[i].name + " is " + pokemonList[i].height + "m" + " Wow, That is big!<br>");
+
+            }
+        }
     }
     return {
-        getAll: getAll,
+        getAll: getAll, // getAll: return all items 
         add: add
-    };
+    }
+
 })()
+
 // Using forEach() loops:
 // Shows forEach loop outside of and below the IIFE. The forEach() loop iterates over each Pokémon in the repositor.
 pokemonList.forEach(function(pokemon) {
