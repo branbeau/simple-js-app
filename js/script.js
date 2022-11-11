3285d6e9f34ca8b7938e14a44d6cd5745fa61c6f
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2022-11-11 13:11:13 -0800

    Updated files

--- js/script.js
+++ js/script.js
@@ -14,12 +14,12 @@
 
 // IIFE - Immediately Invoked Function Expression  
 let pokemonRepository = (function () { // New pokemonRepository variable that holds what the IIFE will return and assign IIFE to the variable
-     // let pokemonList =  [//) PokemonList variable inside pokemonRepository IIFE (Wrap pokemonList array in an IIFE). IIFE wraps around the function - let pokemonRepository = (.
-     //                     // Moved pokemonList from outside of repository. The list previously started on line 1.
-        let repository = [
-        { 
-            "name": "Jigglypuff", 
-            "height": 0.5, 
+    // let pokemonList =  [//) PokemonList variable inside pokemonRepository IIFE (Wrap pokemonList array in an IIFE). IIFE wraps around the function - let pokemonRepository = (.
+    //                     // Moved pokemonList from outside of repository. The list previously started on line 1.
+    let repository = [
+    {
+            "name": "Jigglypuff",
+            "height": 0.5,
             "type": ["normal", "fairy"]
         },
         {
@@ -31,7 +31,7 @@ let pokemonRepository = (function () { // New pokemonRepository variable that ho
             "name": "Lilipup",
             "height": 0.4,
             "type": ["normal"]
-        }
+        },
     ];
 
     //  function add(pokemon) {
@@ -39,44 +39,42 @@ let pokemonRepository = (function () { // New pokemonRepository variable that ho
     // }
 
     function add(pokemon) {
-     if (
-        typeof pokemon === "object" &&
-        "name" in pokemon &&
-        "height" in pokemon &&
-        "types" in pokemon
-    ) {
-      repository.push(pokemon);
-    } else {
-      console.log("pokemon is not correct");
+        if (
+            typeof pokemon === "object" &&
+            "name" in pokemon &&
+            "height" in pokemon &&
+            "types" in pokemon
+        ) {
+            repository.push(pokemon);
+        } else {
+            console.log("pokemon is not correct");
+        }
     }
-  }
-  function getAll() {
-    return repository;
-     // function getAll() {
-     //    return pokemonList;
+
+    function getAll() {
+        return repository;
+        // function getAll() {
+        //    return pokemonList;
     }
-    function addListItem(pokemon){
-    // Lines 43-50 was cut from line 67-73 and indented so code is inside the function 
-        let pokemonList = 
-    document.querySelector(".pokemon-list"); // Created pokemonList variable. Selected the class that was just created in the index.html file - .pokemon-list. Be sure to add dot in front of class name.
+
+    function addListItem(pokemon) {
+        // Lines 43-50 was cut from line 67-73 and indented so code is inside the function 
+        let pokemonList =
+            document.querySelector(".pokemon-list"); // Created pokemonList variable. Selected the class that was just created in the index.html file - .pokemon-list. Be sure to add dot in front of class name.
         let listpokemon = document.createElement("li"); // Have you UL and inside the UL an element was created for the li. 
         let button = document.createElement("button"); // Inside each li, a button will be created. 
         button.innerText = pokemon.name;
-        button.classList.add("button-class") // This styles the button based on the css file. 
-        listpokemon.appendChild(button) // Call list on line 57 to append child and this appends the button
+        button.classList.add("button-class"); // This styles the button based on the css file. 
+        listpokemon.appendChild(button); // Call list on line 57 to append child and this appends the button
         pokemonList.appendChild(listpokemon); // Append listpokemon from line 58 
         button.addEventListener("click", function(event) {
             showDetails(pokemon);
-      });
-  }
-  function showDetails(pokemon) {
-    pokemonRepository.loadDetails(pokemon).then(function () {
-      console.log(pokemon);
-    });
-  }
-     return {
-        getAll: getAll,
+ });
+
+    }
+    return {
         add: add,
+        getAll: getAll,
         addListItem: addListItem // Calling the addListItem from line 42 under return
     };
 
@@ -91,6 +89,6 @@ let pokemonRepository = (function () { // New pokemonRepository variable that ho
 
 console.log(pokemonRepository.getAll());
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
 });
